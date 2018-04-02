80a4b03f71931f10e296ad94884485213a52811e
Wembley Leach <wleac0326@X-Leach-Wembley.local>
2018-04-02 15:08:13 -0400

    Update addMovie mutation to include genres

--- src/main/java/com/example/demo/entities/Movie.java
+++ src/main/java/com/example/demo/entities/Movie.java
@@ -7,95 +7,97 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "movies")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Movie {
-    @Id
-    private String id;
+  @Id
+  private String id;
 
-    private String title;
+  private String title;
 
-    @JsonProperty(value = "overview")
-    private String synopsis;
+  @JsonProperty(value = "overview")
+  private String synopsis;
 
-    private String duration;
+  private String duration;
 
-    @Column(name = "image_url")
-    @JsonProperty(value = "backdrop_path")
-    private String imageUrl;
+  @Column(name = "image_url")
+  @JsonProperty(value = "backdrop_path")
+  private String imageUrl;
 
-    private Integer rating;
+  private Integer rating;
 
-    private String playback;
+  private String playback;
 
-    public Movie() {
-    }
+  public Movie() {
+  }
 
-    public Movie(String id, String title, String synopsis, String duration, String imageUrl, Integer rating, String playback) {
-        this.id = id;
-        this.title = title;
-        this.synopsis = synopsis;
-        this.duration = duration;
-        this.imageUrl = imageUrl;
-        this.rating = rating;
-        this.playback = playback;
-    }
+  public Movie(String id, String title, String synopsis, String duration, String imageUrl, Integer rating, String playback) {
+    this.id = id;
+    this.title = title;
+    this.synopsis = synopsis;
+    this.duration = duration;
+    this.imageUrl = imageUrl;
+    this.rating = rating;
+    this.playback = playback;
+  }
 
-    public String getSynopsis() {
-        return synopsis;
-    }
+  public String getSynopsis() {
+    return synopsis;
+  }
 
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
+  public void setSynopsis(String synopsis) {
+    this.synopsis = synopsis;
+  }
 
-    public String getDuration() {
-        return duration;
-    }
+  public String getDuration() {
+    return duration;
+  }
 
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
+  public void setDuration(String duration) {
+    this.duration = duration;
+  }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
+  public String getImageUrl() {
+    return imageUrl;
+  }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public Integer getRating() {
-        return rating;
-    }
+  public Integer getRating() {
+    return rating;
+  }
 
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
+  public void setRating(Integer rating) {
+    this.rating = rating;
+  }
 
-    public String getPlayback() {
-        return playback;
-    }
+  public String getPlayback() {
+    return playback;
+  }
 
-    public void setPlayback(String playback) {
-        this.playback = playback;
-    }
+  public void setPlayback(String playback) {
+    this.playback = playback;
+  }
+
 }
--- src/main/java/com/example/demo/mutations/AddMovie.java
+++ src/main/java/com/example/demo/mutations/AddMovie.java
@@ -3,43 +3,54 @@ package com.example.demo.mutations;
 import com.creactiviti.spring.boot.starter.graphql.Arguments;
 import com.creactiviti.spring.boot.starter.graphql.Fields;
 import com.creactiviti.spring.boot.starter.graphql.MutationBuilder;
+import com.example.demo.entities.Genre;
 import com.example.demo.entities.Movie;
+import com.example.demo.repositories.GenreRepository;
 import com.example.demo.repositories.MoviesRepository;
 import com.example.demo.types.MovieType;
 import graphql.Scalars;
 import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 
 @Component
 public class AddMovie implements MutationBuilder {
 
-    @Autowired
-    private MoviesRepository moviesRepository;
+  @Autowired
+  private MoviesRepository moviesRepository;
 
-    @Override
-    public void build(GraphQLObjectType.Builder builder) {
-        builder.field(Fields.field("addMovie")
-                .argument(Arguments.notNull(Arguments.stringArgument("title")))
-                .argument(Arguments.stringArgument("synopsis"))
-                .argument(Arguments.stringArgument("duration"))
-                .argument(Arguments.stringArgument("image_url"))
-                .argument(GraphQLArgument.newArgument().name("rating").type(Scalars.GraphQLInt))
-                .argument(Arguments.stringArgument("playback"))
-                .type(MovieType.REF)
-                .dataFetcher(env -> {
-                    Movie movie = new Movie(UUID.randomUUID().toString(),
-                            env.getArgument("title"),
-                            env.getArgument("synopsis"),
-                            env.getArgument("duration"),
-                            env.getArgument("image_url"),
-                            env.getArgument("rating"),
-                            env.getArgument("playback"));
-                    return moviesRepository.save(movie);
-                }));
-    }
+  @Autowired
+  private GenreRepository genreRepository;
+
+  @Override
+  public void build(GraphQLObjectType.Builder builder) {
+    builder.field(Fields.field("addMovie")
+            .argument(Arguments.notNull(Arguments.stringArgument("title")))
+            .argument(Arguments.stringArgument("synopsis"))
+            .argument(Arguments.stringArgument("duration"))
+            .argument(Arguments.stringArgument("image_url"))
+            .argument(GraphQLArgument.newArgument().name("rating").type(Scalars.GraphQLInt))
+            .argument(Arguments.stringArgument("playback"))
+            .argument(GraphQLArgument.newArgument().name("genre").type(GraphQLList.list(Scalars.GraphQLString)))
+            .type(MovieType.REF)
+            .dataFetcher(env -> {
+              Movie movie = new Movie(UUID.randomUUID().toString(),
+                      env.getArgument("title"),
+                      env.getArgument("synopsis"),
+                      env.getArgument("duration"),
+                      env.getArgument("image_url"),
+                      env.getArgument("rating"),
+                      env.getArgument("playback"));
+              movie = moviesRepository.save(movie);
+              for (String genre : (List<String>) env.getArgument("genre")) {
+                genreRepository.save(new Genre(movie.getId(), genre));
+              }
+              return movie;
+            }));
+  }
 }
